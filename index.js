f3e43ebf182fd3af19d3dc7cdd7ca44e4737bb1c
JoseSidnei <juniorzeh@live.com>
2021-05-13 00:00:56 -0300

    Utilizando fs

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
 const http = require('http');
+const fs = require('fs');
+
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
-const server = http.createServer((reg,res)=>{
+//criar novo arquivo
+/*
+fs.writeFile('danki.txt','teste danki code',function(err){
+    if(err) throw err;
+    console.log('o arquivo foi criado com sucesso!');
+})
+*/
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
+//cria novo arquivo ou insere o contaúdo depois do que ja existe
+
+fs.appendFile('danki.txt',"\nOutro conteúdo",(err)=>{
+    if(err) throw err;
+    console.log('Salvo novamente com sucesso!');
+})
+
+const server = http.createServer((req,res)=>{
+
+    if(req.url == '/danki'){
+        
+    fs.readFile('index.html',function(err,data){
+
+        fs.appendFile('danki.txt',"\nOutro conteúdo",(err)=>{
+            if(err) throw err;
+            console.log('Salvo novamente com sucesso!');
+        })
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write(data);
+        return res.end();
+    })
+
+    }else{
+        //return res.end();
+    }
 
 
-    res.end("Hello World!");
+
+
 })
 
 server.listen(port,hostname,()=>{
